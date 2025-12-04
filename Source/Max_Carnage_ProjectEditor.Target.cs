f4980b8b9e12f61b8b7c57dc781165b34afbf7ed
Eduardo Gonzalez <eduardo.gonzalez1214@outlook.com>
2025-12-03 18:04:55 -0800

    quick2
    the actual change,  copys op

--- Source/Max_Carnage_ProjectEditor.Target.cs
+++ Source/Max_Carnage_ProjectEditor.Target.cs
@@ -3,13 +3,14 @@
 using UnrealBuildTool;
 using System.Collections.Generic;
 
-public class Max_Carnage_ProjectEditorTarget : TargetRules
-{
-	public Max_Carnage_ProjectEditorTarget(TargetInfo Target) : base(Target)
-	{
-		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+public class Max_Carnage_ProjectEditorTarget : TargetRules {
+    public Max_Carnage_ProjectEditorTarget(TargetInfo Target) : base(Target) {
+        Type = TargetType.Editor;
 
-		ExtraModuleNames.AddRange( new string[] { "Max_Carnage_Project" } );
-	}
+        DefaultBuildSettings = BuildSettingsVersion.Latest;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+
+        ExtraModuleNames.AddRange(new string[] { "Max_Carnage_Project" });
+    }
 }
+
